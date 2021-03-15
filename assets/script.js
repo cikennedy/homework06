28215a9907f4e43a5fdbe4ca96ae87f5aa0cc082
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-03-15 16:10:56 -0500

    added bootstrap, messing around with js and html from examples in class. adjusting everything as needed. still needs work

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+//Added API key as a var as it will be necessary to call the API 
+var apiKey = "1ab858a5505d5908820588cc1ee2f9e5";
+
+//current weather API = api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+//api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+
+//5 day weather API = api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+//api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}
+//api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
+
+//Code below is from exercise 28 - double check everything makes sense and works 
 var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit(event) {
