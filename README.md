b96cdf2f867354cf79e91bfdcdc7bdd877ffb792
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-03-15 22:37:15 -0500

    chagned js, css, html, readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homework06
+
+Description: 
+
+The application is a weather dashboard that allows the user to search for a city's weather. The user will be met with the city's current weather, UV index, and five day forecast. The user's search history will be saved on the page. 
+
+
--- assets/script.js
+++ assets/script.js
@@ -9,23 +9,5 @@ var apiKey = "1ab858a5505d5908820588cc1ee2f9e5";
 //api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}
 //api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
 
-//Code below is from exercise 28 - double check everything makes sense and works 
-var searchFormEl = document.querySelector('#search-form');
+//Add UV index API data 
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
-
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
-
-  location.assign(queryString);
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
