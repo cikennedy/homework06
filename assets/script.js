87eb41eb5da33339a75815d481a9734e5e53d7c4
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 18:35:04 -0500

    updating script.js with revised code

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,143 @@
-//Added API key as a var as it will be necessary to call the API 
+// Set variables for API key, url, and array of search history
 var apiKey = "1ab858a5505d5908820588cc1ee2f9e5";
+var apiURL = "https://api.openweathermap.org";
+var searchHistory = [];
 
-//current weather API = api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
-//api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+// create query selector references here 
 
-//5 day weather API = api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-//api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key}
-//api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
+// Current weather function
+const displayCurrentWeather = (city, weather) => {
+    var date = dayjs().format('MM/DD/YYYY');
+    var temp = weather.temp;
+    var wind_speed = weather.wind_speed;
+    var uvi = weather.uvi;
+    var icon = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
 
-//Add UV index API data 
+    // Create elements 
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h3');
+    var weatherIcon = document.createElement('img');
+    var tempElement = document.createElement('p');
+    var windElement = document.createElement('p');
+    var humidityElement = document.createElement('p');
+    var uvElement = document.createElement('p');
+    var uviBadge = document.createElement('button');
 
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city 
-*/
+    card.setAttribute('class', 'card');
+    cardBody.setAttribute('class', 'card-body');
+    card.append(cardBody);
+  
+    heading.setAttribute('class', 'h3 card-title');
+    tempElement.setAttribute('class', 'card-text');
+    windElement.setAttribute('class', 'card-text');
+    humidityElement.setAttribute('class', 'card-text');
+  
+    heading.textContent = `${city} (${date})`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    weatherIcon.setAttribute('class', 'weather-img');
+    heading.append(weatherIcon);
+    tempElement.textContent = `Temp: ${temp}°F`;
+    windElement.textContent = `Wind: ${wind_speed} MPH`;
+    humidityElement.textContent = `Humidity: ${humidity} %`;
+    cardBody.append(heading, tempElement, windElement, humidityElement);
+  
+    uvElement.textContent = 'UV Index: ';
+    uviBadge.classList.add('btn', 'btn-sm');
+  
+    if (uvi < 3) {
+      uviBadge.classList.add('btn-success');
+    } else if (uvi < 7) {
+      uviBadge.classList.add('btn-warning');
+    } else {
+      uviBadge.classList.add('btn-danger');
+    }
+  
+    uviBadge.textContent = uvi;
+    uvEl.append(uviBadge);
+    cardBody.append(uvEl);
+  
+    todayContainer.innerHTML = '';
+    todayContainer.append(card);
 
-// Set variable for API key
-var weatherAPI = "1ab858a5505d5908820588cc1ee2f9e5";
+}
 
+// Function for a forecast card 
+const showForecastCard = (forecast) => {
+
+    var icon = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+    var unixTs = forecast.dt;
+    var temp = forecast.temp.day;
+    var { humidity } = forecast;
+    var wind_speed = forecast.wind_speed;
+
+    var col = document.createElement('div');
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var cardTitle = document.createElement('h5');
+    var weatherIcon = document.createElement('img');
+    var tempElement = document.createElement('p');
+    var windElement = document.createElement('p');
+    var humidityElement = document.createElement('p');
+
+    col.append(card);
+    card.append(cardBody);
+    cardBody.append(cardTitle, weatherIcon, tempElement, windElement, humidityElement);
+
+    col.setAttribute('class', 'col-md');
+    col.classList.add('five-day-card');
+    card.setAttribute('class', 'card bg-primary h-100 text-white');
+    cardBody.setAttribute('class', 'card-body p-2');
+    cardTitle.setAttribute('class', 'card-title');
+    tempElement.setAttribute('class', 'card-text');
+    windElement.setAttribute('class', 'card-text');
+    humidityElement.setAttribute('class', 'card-text');
+
+    // Add content to elements
+    cardTitle.textContent = dayjs.unix(unixTs).format('M/D/YYYY');
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconDescription);
+    tempElement.textContent = `Temp: ${temp} °F`;
+    windElement.textContent = `Wind: ${wind_speed} MPH`;
+    humidityElement.textContent = `Humidity: ${humidity} %`;
+
+    forecastContainer.append(col);
+
+}
+
+// Create function that will show the search history 
+const displaySearchHistory = () => {
+    searchHistoryContainer.innerHTML = '';
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+        var btn = document.createElement('btn');
+        btn.setAttribute('type', 'btn');
+        btn.setAttribute('aria-controls', 'today forecast');
+        btn.classList.add('history-btn', 'btn-history');
+    
+        // `data-search` allows access to city name when click handler is invoked
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+// Create function that updates history 
+const updateHistory = (search) => {
+    if (searchHistory.indexOf(search) !== -1) {
+      return;
+    }
+    searchHistory.push(search);
+    // Updates search history 
+    localStorage.setItem('search-history', JSON.stringify(searchHistory));
+    displaySearchHistory();
+}
+
+// Function to get search history from local storage
+const retrieveHistory = () => {
+    var historyStorage = localStorage.getItem('search-history');
+    if (historyStorage) {
+      searchHistory = JSON.parse(historyStorage);
+    }
+    displaySearchHistory();
+}
